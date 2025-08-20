1b33f0fa0b8fb255e08912da18f8ccf32545df50
michael <michaelferreiraalves2001@gmail.com>
2025-08-20 01:01:08 -0300

    create

---
+++ src/main/java/shop/ferrerimha/intensivo_spring/IntensivoSpringApplication.java
@@ -0,0 +1,13 @@
+package shop.ferrerimha.intensivo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/ferrerimha/intensivo_spring/IntensivoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.ferrerimha.intensivo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
